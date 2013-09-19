f6f765b20435f068fb91931c53d1c8136f5513c6
MCodyB <mcodyb@gmail.com>
2013-09-19 10:29:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whispersapp::Application.config.secret_key_base = '34f4bfd4ea296c9680b2b50ce75f76f34d49c023d1ad3ef944c95556f9df46156de88252c3ea378b54e7abef3053e6038ad0d3b8180a0b8a54191192cf222eae'
